d64d609ececbce6796a0a479a975004d0eeb9e8c
DocBrownAg <apassadore@gmail.com>
2025-02-24 13:38:44 +0100

    V240225

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,9 @@
 const menu = document.getElementById('menu');
+const footer = document.getElementById('footer');
+let year = new Date().getFullYear();
 
 if (menu) {
-    menu.innerHTML = DOMPurify.sanitize(`
+  menu.innerHTML = DOMPurify.sanitize(`
          <header class="header-wrap header_1">
       <div class="container">
         <div class="row">
@@ -139,4 +141,101 @@ if (menu) {
       </div>
     </header>
         `);
+}
+
+if (footer) {
+  footer.innerHTML = DOMPurify.sanitize(`
+      <footer
+      class="footer_wrapper footer-1 section-top bg-center bg-cover"
+      style="background-image: url(./assets/img/f_1900x700_03.png)"
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 pe-2 pe-sm-5 pe-lg-0 mt-4 mt-md-0">
+            <div class="single_footer_widget">
+              <div class="logo_footer">
+                <a href="index.html"
+                  ><img src="./assets/img/logo.png" alt=""
+                /></a>
+              </div>
+              <p>
+                Asesoramiento legal y administración de fincas con garantía de excelencia<br><br>
+                Gestionamos el mantenimiento de todos los servicios y prestaciones comunes de su comunidad, con el objetivo final de incrementar el valor de su vivienda.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 ps-2 ps-lg-5 mt-4 mt-md-0">
+            <div class="single_footer_widget">
+              <h4>Enlaces rápidos</h4>
+              <div class="single_footer_link">
+                <ul>
+                  <li>
+                    <a href="nosotros.html"
+                      ><i class="far fa-angle-double-right"></i>Nosotros</a>
+                  </li>
+                  <li>
+                    <a href="servicios.html"
+                      ><i class="far fa-angle-double-right"></i>Servicios</a>
+                  </li>
+                  <li>
+                    <a href="blog.html"
+                      ><i class="far fa-angle-double-right"></i>Prensa</a>
+                  </li>
+                  <li>
+                    <a href="https://private.tucomunidad.com/#/login"
+                      ><i class="far fa-angle-double-right"></i>Area privada</a>
+                  </li>
+                  <li>
+                    <a href="index.html"
+                      ><i class="far fa-angle-double-right"></i>Nuestros abogados</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
+            <div class="single_footer_widget">
+              <h4>Financiado por</h4>
+              <div class="single_footer_link">
+                <img class="logo-union" src="/assets/img/f_resiliencia.jpeg" alt="Plan de Recuperación, transformación y resiliencia">
+                <img class="logo-union" src="/assets/img/f_unionEuropea.jpeg" alt="Financiado por la Unión Europea">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
+            <div class="single_footer_widget">
+              <h4>Oficina Central</h4>
+              <div class="single_footer_address">
+                <div class="single_address">
+                  <div class="icon mt-6">
+                    <i class="fal fa-map-marker-alt"></i>
+                  </div>
+                  <p>Av. de la Constitución 34 <br />Beiro, 18012 Granada, España</p>
+                </div>
+                <div class="single_address">
+                  <div class="icon mt-6"><i class="fal fa-envelope"></i></div>
+                  <a href="mailto:info@serranoytorres.com">info@serranoytorres.com</a>
+                </div>
+                <div class="single_address">
+                  <div class="icon"><i class="icon-phone"></i></div>
+                  <a href="tel:+34958283829">958 283 829</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div class="footer_bottom_widget py-3">
+          <span
+            ><a href="privacidad.html">Privacidad</a> |
+            <a href="https://serranoytorres.com/webmail/log-in">Webmail</a></span
+            >
+          <span
+            >© ${year} <a id="f-stlink" href="index.html">Serrano & Torres </a> | Todos los derechos reservados</span>
+        </div>
+      </div>
+    </footer> 
+    `);
 }
